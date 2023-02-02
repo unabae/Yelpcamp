b30219941a15941c15b47a71354ffc13e05331fb
Laurence <laurenceunabia@gmail.com>
2023-02-02 12:11:53 +0800

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary, 
+    params: {
     folder: 'Yelpcamp',
     allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {
