59ccbfc12266f3cc2f3bea3343d8eade91ef2776
Laurence <laurenceunabia@gmail.com>
2023-07-28 14:54:57 +0800

    added mongo sanitize

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -37,6 +38,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({replaceWith: '_'}));
+
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -59,7 +62,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
